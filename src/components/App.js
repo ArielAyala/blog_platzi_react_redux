642978beb1f4deacd7bfddf3eb4dc10d21a864c2
Ariel <arielayalamail@gmail.com>
2019-06-19 07:25:05 -0400

    Cambiando el componenente funcional a componente clase y se agrega el estado

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  const ponerFilas = () => [
-    <tr>
-      <td>Ariel</td>
-      <td>ariel@prueba.com</td>
-      <td>prueba.com</td>
-    </tr>,
-    <tr>
-      <td>Platzi</td>
-      <td>platzi@prueba.com</td>
-      <td>Platzi.com</td>
-    </tr>
-  ];
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [
+        {
+          nombre: "Ariel",
+          correo: "ariel@prueba.com",
+          enlace: "ariel.com"
+        },
+        {
+          nombre: "Platzi",
+          correo: "platzi@prueba.com",
+          enlace: "Platzi.com"
+        }
+      ]
+    };
+  }
 
-  return (
-    <div className="margen">
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{ponerFilas()}</tbody>
-      </table>
-    </div>
-  );
-};
+  ponerFilas = () =>
+    this.state.usuarios.map(usuario => (
+      <tr>
+        <td>{usuario.nombre}</td>
+        <td>{usuario.correo}</td>
+        <td>{usuario.enlace}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default App;

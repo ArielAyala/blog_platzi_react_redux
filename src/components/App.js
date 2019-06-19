784171d884ff2feb5438a4710ee2cbdf3130c8d0
Ariel <arielayalamail@gmail.com>
2019-06-19 06:43:23 -0400

    Agregando estilos a la tabla

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,28 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      hola
+    <div className="margen">
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Ariel</td>
+            <td>ariel@prueba.com</td>
+            <td>prueba.com</td>
+          </tr>
+          <tr>
+            <td>Platzi</td>
+            <td>platzi@prueba.com</td>
+            <td>Platzi.com</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };

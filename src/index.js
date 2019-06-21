bd92c9cf77acf91a39fd415270ab9bb97f6952b5
Ariel <arielayalamail@gmail.com>
2019-06-21 07:47:26 -0400

    Se envuelve la aplicación con el Provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers/index";
 
 const store = createStore(
-    {}, //Todos los reducers
-    {} // Estado inicial
-)
+  reducers, //Todos los reducers
+  {} // Estado inicial
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

2679303cec87dc0eee386895f0888e71140c95a3
Ariel <arielayalamail@gmail.com>
2019-06-21 07:24:24 -0400

    Agregado del store a la app

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+    {}, //Todos los reducers
+    {} // Estado inicial
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 

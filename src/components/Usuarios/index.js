61948be41fcadb53ce4e0c4ca90c9d605cce93d9
Ariel <arielayalamail@gmail.com>
2019-06-21 16:00:32 -0400

    Conectando los reducers a un componente

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,26 +1,16 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { connect } from "react-redux";
 
 class Usuarios extends Component {
-  constructor() {
-    super();
-    this.state = {
-      usuarios: []
-    };
-  }
-
-  async componentDidMount() {
-    const respuesta = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-
-    this.setState({
-      usuarios: respuesta.data
-    });
-  }
+  // async componentDidMount() {
+  // 	const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+  // 	this.setState({
+  // 		usuarios: respuesta.data
+  // 	});
+  // }
 
   ponerFilas = () =>
-    this.state.usuarios.map(usuario => (
+    this.props.usuarios.map(usuario => (
       <tr key={usuario.id}>
         <td>{usuario.name}</td>
         <td>{usuario.email}</td>
@@ -46,4 +36,8 @@ class Usuarios extends Component {
   }
 }
 
-export default Usuarios;
+const mapStateToProps = reducers => {
+  return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps,{/*Accion Creator*/})(Usuarios);

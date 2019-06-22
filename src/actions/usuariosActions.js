fbb180b92696bbdb4ac389046388338cc0d5548a
Ariel <arielayalamail@gmail.com>
2019-06-22 07:30:57 -0400

    Agregando try catch a la petición

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -2,12 +2,13 @@ import axios from "axios";
 import { TRAER_TODOS } from "../types/usuariosTypes";
 
 export const traerTodos = () => async dispatch => {
-  const respuesta = await axios.get(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-
-  dispatch({
-    type: TRAER_TODOS,
-    payload: respuesta.data
-  });
+  try {
+    const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users");
+    dispatch({
+      type: TRAER_TODOS,
+      payload: respuesta.data
+    });
+  } catch (error) {
+    console.log('Error: ' ,error.message)
+  }
 };

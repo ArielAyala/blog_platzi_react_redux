91d4ab7e8ed8d81b30d8ffc30ae326ad778d77ec
ArielAyala <arielayalamail@gmail.com>
2019-06-22 18:22:31 -0400

    Agregando de estados 'Cargando' y 'error'

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,14 +1,24 @@
 import axios from "axios";
-import { TRAER_TODOS } from "../types/usuariosTypes";
+import { TRAER_TODOS, CARGANDO, ERROR } from "../types/usuariosTypes";
 
 export const traerTodos = () => async dispatch => {
+  dispatch({
+    type: CARGANDO
+  });
+
   try {
-    const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const respuesta = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
     dispatch({
       type: TRAER_TODOS,
       payload: respuesta.data
     });
   } catch (error) {
-    console.log('Error: ' ,error.message)
+    console.log("Error: ", error.message);
+    dispatch({
+      type: ERROR,
+      payload: error.message
+    });
   }
 };
--- src/reducers/usuariosReducer.js
+++ src/reducers/usuariosReducer.js
@@ -1,13 +1,19 @@
-import { TRAER_TODOS } from "../types/usuariosTypes";
+import { TRAER_TODOS, CARGANDO, ERROR } from "../types/usuariosTypes";
 
 const INITIAL_STATE = {
-  usuarios: []
+  usuarios: [],
+  cargando: false,
+  error: ""
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TRAER_TODOS:
-      return { ...state, usuarios: action.payload };
+      return { ...state, usuarios: action.payload, cargando: false };
+    case CARGANDO:
+      return { ...state, cargando: true };
+    case ERROR:
+      return { ...state, error: action.payload, cargando: false };
     default:
       return state;
   }

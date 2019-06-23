7d1b551e3207474802e3d55028e04af5d792cd7c
ArielAyala <arielayalamail@gmail.com>
2019-06-23 00:53:43 -0400

    Compartir reducer

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,13 +1,27 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as usuariosActions from '../../actions/usuariosActions';
 
 class Publicaciones extends Component {
-    render(){
-        return(
-            <div>
-                { this.props.match.params.key }
-            </div>
-        )
+  componentDidMount() {
+    if (!this.props.usuarios.length) {
+        this.props.traerTodos();
     }
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1>Publicaciones de</h1>
+        {this.props.match.params.key}
+      </div>
+    );
+  }
 }
 
-export default Publicaciones;
+const mapStateToProps = reducers => {
+  return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps,usuariosActions)(Publicaciones);

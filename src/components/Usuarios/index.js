709a2e5201504b7dbbcc4014bc8db34ca0b90ebc
ArielAyala <arielayalamail@gmail.com>
2019-06-22 20:26:56 -0400

    Componentizado de Tabla

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
 import Spinner from "../General/Spinner";
 import Fatal from "../General/Fatal";
+import Tabla from "./Tabla";
 
 class Usuarios extends Component {
   componentDidMount() {
@@ -15,36 +16,21 @@ class Usuarios extends Component {
       return <Spinner />;
     }
     if (this.props.error) {
-      return <Fatal mensaje ={this.props.error} />;
+      return <Fatal mensaje={this.props.error} />;
     }
-    if (this.props.error){
+    if (this.props.error) {
       return this.props.error;
     }
-    return (
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{this.ponerFilas()}</tbody>
-      </table>
-    );
+    return <Tabla />;
   };
 
-  ponerFilas = () =>
-    this.props.usuarios.map(usuario => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ));
-
   render() {
-    return <div>{this.ponerContenido()}</div>;
+    return (
+      <div>
+        <h1>Usuarios</h1>
+        {this.ponerContenido()}
+      </div>
+    );
   }
 }
 

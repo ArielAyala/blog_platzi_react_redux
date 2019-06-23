53454e845e594b361aa2f007b98af52ff3dae60a
ArielAyala <arielayalamail@gmail.com>
2019-06-23 15:38:03 -0400

    Evitar sobreescritura

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Spinner from "../General/Spinner";
+import Fatal from "../General/Fatal";
+
 import * as usuariosActions from "../../actions/usuariosActions";
 import * as publicacionesActions from "../../actions/publicacionesActions";
 
@@ -8,17 +11,52 @@ const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 
 class Publicaciones extends Component {
   async componentDidMount() {
+    const {
+      usuariosTraerTodos,
+      publicacionesTraerPorUsuario,
+      match: {
+        params: { key }
+      }
+    } = this.props;
+
     if (!this.props.usuariosReducer.usuarios.length) {
-      await this.props.usuariosTraerTodos();
+      await usuariosTraerTodos();
     }
-    this.props.publicacionesTraerPorUsuario(this.props.match.params.key);
+
+    if (this.props.usuariosReducer.error) {
+      return;
+    }
+
+    if (!("publicaciones_key" in this.props.usuariosReducer.usuarios[key])) {
+      publicacionesTraerPorUsuario(key);
+    }
   }
 
+  ponerUsuario = () => {
+    const {
+      usuariosReducer,
+      match: {
+        params: { key }
+      }
+    } = this.props;
+
+    if (usuariosReducer.error) {
+      return <Fatal mensaje={usuariosReducer.error} />;
+    }
+
+    if (!usuariosReducer.usuarios.length || usuariosReducer.cargando) {
+      return <Spinner />;
+    }
+
+    const nombre = usuariosReducer.usuarios[key].name;
+
+    return <h1>Publicaciones de {nombre}</h1>;
+  };
+
   render() {
-    console.log(this.props);
     return (
       <div>
-        <h1>Publicaciones de</h1>
+        {this.ponerUsuario()}
         {this.props.match.params.key}
       </div>
     );

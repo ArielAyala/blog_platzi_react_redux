f94b599c98e346aa021f367c004529b455ee3063
ArielAyala <arielayalamail@gmail.com>
2019-06-23 12:51:43 -0400

    Inmutabilidad

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -4,6 +4,9 @@ import {
   ERROR,
   TRAER_POR_USUARIO
 } from "../types/publicacionesTypes";
+import * as usuariosTypes from "../types/usuariosTypes";
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = key => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
@@ -14,6 +17,18 @@ export const traerPorUsuario = key => async (dispatch, getState) => {
     `https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
   );
   const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key
+  };
+
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados
+  });
 
   dispatch({
     type: TRAER_POR_USUARIO,

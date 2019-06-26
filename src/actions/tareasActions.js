a238c5a26514c7d18afaa028151fd3f6464559dc
Ariel <arielayalamail@gmail.com>
2019-06-26 06:05:30 -0400

    Normalizando datos de tareas

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -10,15 +10,27 @@ export const traerTodas = () => async dispatch => {
     const respuesta = await axios.get(
       "https://jsonplaceholder.typicode.com/todos"
     );
+
+    const tareas = {};
+    respuesta.data.map(
+      tar =>
+        (tareas[tar.userId] = {
+          ...tareas[tar.userId],
+          [tar.id]: {
+            ...tar
+          }
+        })
+    );
+
     dispatch({
       type: TRAER_TODAS,
-      payload: respuesta.data
+      payload: tareas
     });
   } catch (error) {
     console.log("Error: ", error.message);
     dispatch({
       type: ERROR,
-      payload: 'Información de tareas no disponible'
+      payload: "Información de tareas no disponible"
     });
   }
 };
